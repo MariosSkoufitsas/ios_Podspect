d955b5b46f2332161d8ce7fbdb5dde8bc8ecaa35
MariosSkoufitsas <49339658+MariosSkoufitsas@users.noreply.github.com>
2023-04-26 16:55:45 +0300

    Update and rename MyLibrary.podspec to MyLibrary.swift

---
+++ MyLibrary.swift
@@ -0,0 +1,22 @@
+
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "AdverticalTV-ads",
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v13),
+    ],
+    products: [
+        .library(name: "AdverticalTV-ads", targets: ["AdverticalTV_ads"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AdverticalTV_ads",
+            url: "https://github.com/arx-net/AdverticalTV-ads-iOS/releases/download/1.1.0/xcframework.zip",
+            checksum: "ace1a7a6d21580d4b1f1bb3f86f2af58fcbf65547aa46ce3539f1ad32a1cf2cd"
+        )
+    ],
+    swiftLanguageVersions: [.v4, .v4_2, .v5]
+)
